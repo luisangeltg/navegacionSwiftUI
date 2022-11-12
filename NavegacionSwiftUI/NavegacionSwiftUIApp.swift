ccd49565d65c9a015b66fb9875ad952a457abd4d
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2022-11-12 11:53:18 -0600

    Initial Commit

---
+++ NavegacionSwiftUI/NavegacionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavegacionSwiftUIApp.swift
+//  NavegacionSwiftUI
+//
+//  Created by Luis Angel Torres G on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NavegacionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
